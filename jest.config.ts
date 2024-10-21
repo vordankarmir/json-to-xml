45d457f05819af72eb2e68c851d989d6f1771c3c
Henry <henrygrigoryan5@gmail.com>
2024-10-21 14:06:48 +0400

    support es6 & commonjs modules, add test coverage

---
+++ jest.config.ts
@@ -0,0 +1,6 @@
+export default {
+  preset: "ts-jest",
+  testEnvironment: "node",
+  moduleFileExtensions: ["ts", "tsx", "js", "jsx", "json", "node"],
+  roots: ["<rootDir>/src", "<rootDir>/tests"],
+};
---
+++ lib/cjs/types/index.d.ts
@@ -0,0 +1,1 @@
+export default function OBJtoXML(obj: any): string;
